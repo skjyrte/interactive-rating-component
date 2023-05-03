f523edde24b664f3d6afcf075d525c6da194e412
skjyrte <103749024+skjyrte@users.noreply.github.com>
2023-05-03 21:30:07 +0200

    formatting fixed

--- script/script.js
+++ script/script.js
@@ -1,36 +1,44 @@
 //rating button listener
-let rating = undefined;
+let rating;
 const container = document.querySelector(".rank");
 let previousTarget;
 
-container.addEventListener('click', (event) => {if (event.target.value!==undefined) {rating = event.target.value;
-    if (previousTarget!==undefined){
-    previousTarget.classList.remove("active");}
+container.addEventListener("click", (event) => {
+  if (event.target.value !== undefined) {
+    rating = event.target.value;
+    if (previousTarget !== undefined) {
+      previousTarget.classList.remove("active");
+    }
     event.target.classList.add("active");
-    previousTarget = event.target}});
+    previousTarget = event.target;
+  }
+});
 
 //submit button listener
 const submit = document.querySelector(".submitButton");
-submit.addEventListener('click', () => {if (rating!==undefined) return change()});
+submit.addEventListener("click", () => {
+  if (rating !== undefined) return change();
+});
 
 function change() {
-    //picture replacement
-    document.querySelector(".image > img").src = "./images/illustration-thank-you.svg" //child combinator
+  //picture replacement
+  document.querySelector(".image > img").src =
+    "./images/illustration-thank-you.svg"; //child combinator
 
-    //text replacement
-    const headerNode = document.querySelector("h1");
-    headerNode.innerHTML = "Thank you!";
-    document.querySelector(".para").innerHTML = "We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!";
+  //text replacement
+  const headerNode = document.querySelector("h1");
+  headerNode.innerHTML = "Thank you!";
+  document.querySelector(".para").innerHTML =
+    "We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!";
 
-    //rating element
-    const ratingNode = document.createElement("p");
-    const textNode = document.createTextNode(`You selected ${rating} out of 5`)
-    ratingNode.appendChild(textNode);
-    const parentNode = document.querySelector(".frame");
-    parentNode.insertBefore(ratingNode, headerNode);
+  //rating element
+  const ratingNode = document.createElement("p");
+  const textNode = document.createTextNode(`You selected ${rating} out of 5`);
+  ratingNode.appendChild(textNode);
+  const parentNode = document.querySelector(".frame");
+  parentNode.insertBefore(ratingNode, headerNode);
 
-    //removal of unnecessary elements
-    container.remove();
-    submit.remove();
+  //removal of unnecessary elements
+  container.remove();
+  submit.remove();
 }
-
