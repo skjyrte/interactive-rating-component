e00d3c3e630693a555f0e4727985381eeed82522
unknown <wasylewiczkrzysztof@gmail.com>
2024-01-07 12:15:54 +0100

    Readme added, minor updates

---
+++ README.md
@@ -0,0 +1,20 @@
+# Interactive rating component.
+
+SPA for providing 1-5 rating with on-submit message.
+
+## Live preview:
+
+https://interactive-rating-component-oi7o.onrender.com
+
+## Template for the project:
+
+https://www.frontendmentor.io/challenges/interactive-rating-component-koxpeBUmI
+
+## Features:
+
+- JS
+- CSS
+
+## Getting started:
+
+1. App requires no build, run the `index.html`
--- script/script.js
+++ script/script.js
@@ -1,4 +1,3 @@
-//rating button listener
 let rating;
 const container = document.querySelector(".rank");
 let previousTarget;
@@ -14,24 +13,19 @@ container.addEventListener("click", (event) => {
   }
 });
 
-//submit button listener
 const submit = document.querySelector(".submitButton");
 submit.addEventListener("click", () => {
   if (rating !== undefined) return change();
 });
 
 function change() {
-  //picture replacement
-  document.querySelector(".image > img").src =
-    "./images/illustration-thank-you.svg"; //child combinator
+  document.querySelector(".image > img").src = "./images/illustration-thank-you.svg";
 
-  //text replacement
   const headerNode = document.querySelector("h1");
   headerNode.innerHTML = "Thank you!";
   document.querySelector(".para").innerHTML =
     "We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!";
 
-  //rating element
   const ratingNode = document.createElement("p");
   const textNode = document.createTextNode(`You selected ${rating} out of 5`);
   ratingNode.appendChild(textNode);
@@ -39,10 +33,8 @@ function change() {
   parentNode.insertBefore(ratingNode, headerNode);
   ratingNode.classList.add("ratingResult");
 
-  //removal of unnecessary elements
   container.remove();
   submit.remove();
 
-  //class for styling of second window
   parentNode.classList.add("frameFinal");
 }
